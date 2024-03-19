faddc6b08640535d30ed19b7e399d1d41f97099a
NTDEV SHOP <>
2024-03-19 10:52:11 +0700

    https://ntgamesdevshop.github.io/ntgame/
    https://ntgamesdevshop.github.io/ntgame/

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ntgame
- ntgame
---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Check login status
+  var loggedIn = false; // Change this value based on login status
+  var loginStatusElement = document.getElementById('login-status');
+
+  if (loggedIn) {
+    // If logged in, display Discord user info
+    loginStatusElement.innerHTML = 'Logged in as: Your Discord Username';
+  } else {
+    // If not logged in, display login prompt
+    loginStatusElement.innerHTML = 'Please <a href="#">login</a>';
+  }
+});
+
+document.getElementById('login-btn').addEventListener('click', function() {
+  window.location.href = 'https://discord.com/oauth2/authorize?client_id=1212026266291806288&response_type=code&redirect_uri=https%3A%2F%2Fntgamesdevshop.github.io%2Fntgame%2F&scope=identify';
+});
+
