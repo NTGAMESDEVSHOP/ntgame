4522be79fb404e03da75cf65b2b688fd86a5d4d9
ntdevshop <153606978+NTGAMESDEVSHOP@users.noreply.github.com>
2024-03-19 10:06:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('login-btn').addEventListener('click', function() {
   // Redirect user to Discord OAuth login page
-  window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=1212026266291806288&redirect_uri=https%3A%2F%2Fdiscord.com%2Foauth2%2Fauthorize%3Fclient_id%3D1212026266291806288%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fntgamesdevshop.github.io%252Fntgame%252F%26scope%3Didentify&response_type=code&scope=identify';
+  window.location.href = 'https://discord.com/oauth2/authorize?client_id=1212026266291806288&response_type=code&redirect_uri=https%3A%2F%2Fntgamesdevshop.github.io%2Fntgame%2F&scope=identify';
 });
