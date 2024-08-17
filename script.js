85c1e0ddb497c887ef4e5d6b61c84420120e8298
ntdevshop <153606978+NTGAMESDEVSHOP@users.noreply.github.com>
2024-08-17 23:59:25 +0700

    Update script.js
    Signed-off-by: ntdevshop <153606978+NTGAMESDEVSHOP@users.noreply.github.com>

--- script.js
+++ script.js
@@ -14,5 +14,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.getElementById("login-btn").addEventListener("click", function () {
   window.location.href =
-    "https://discord.com/oauth2/authorize?client_id=1212026266291806288&response_type=code&redirect_uri=https%3A%2F%2Fntgamesdevshop.github.io%2Fntgame%2F&scope=identify";
+    "https://discord.com/oauth2/authorize?client_id=1212026266291806288&&response_type=code&redirect_uri=https%3A%2F%2Fntgamesdevshop.github.io%2Fntgame%2F&scope=identify";
 });
