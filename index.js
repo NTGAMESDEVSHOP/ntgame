fc2cda964c579a403e82e27f38a6e347786f2168
ntdevshop <153606978+NTGAMESDEVSHOP@users.noreply.github.com>
2024-08-18 07:14:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ app.get('/', (request, response) => {
 });
 
 app.listen(port, () => console.log(`App listening at https://ntgamesdevshop.github.io/ntgame`));
+
+function generateRandomString() {
+	let randomString = '';
+	const randomNumber = Math.floor(Math.random() * 10);
+
+	for (let i = 0; i < 20 + randomNumber; i++) {
+		randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94));
+	}
+
+	return randomString;
+}
+
+window.onload = () => {
+	// ...
+	if (!accessToken) {
+		const randomString = generateRandomString();
+		localStorage.setItem('oauth-state', randomString);
+
+		document.getElementById('login').href += `&state=${btoa(randomString)}`;
+		return (document.getElementById('login').style.display = 'block');
+	}
+};
